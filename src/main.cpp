89b0c116139e69de62e2c331c6614de1c1d283de
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-04-17 22:37:14 -0300

    adicionado menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <ostream>
 using std::cout;
 using std::endl;
 
-int main(void) {
-  cout << "hello world" << endl;
+void menu() {
+  cout << "caxeiro viajante - projeto da disciplina de comp. grafica" << endl;
+  cout << "Uso: caxe <arquivo de entrada>" << endl;
+}
+
+int main(int argc, char const *argv[]) {
+  if (argc == 1) {
+    menu();
+  }
+
   return 0;
 }
